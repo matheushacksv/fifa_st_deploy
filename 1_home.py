a7980f3e6cea21b2779eb79177dc2432c5b515b2
matheushacksv <matheushacksv@gmail.com>
2025-03-03 17:32:45 -0300

    Update 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -13,9 +13,7 @@ if 'data' not in st.session_state:
 
 st.markdown('# FIFA 2023 OFFICIAL DATASET!')
 
-btn = st.button('Acesse os dados no Kaggle!')
-if btn:
-    webbrowser.open_new_tab('https://www.kaggle.com/datasets/bryanb/fifa-player-stats-database')
+btn = st.link_button('Acesse os dados no Kaggle!', 'https://www.kaggle.com/datasets/bryanb/fifa-player-stats-database')
 
 st.markdown('''
             O conjunto de dados contém mais de 17 mil jogadores únicos e mais de 60 colunas, incluindo informações gerais e todos os KPIs que o famoso videogame oferece. Como o cenário dos esportes eletrônicos continua crescendo, especialmente no FIFA, pensei que isso poderia ser útil para a comunidade (usuários do Kaggle e/ou jogadores).\n
